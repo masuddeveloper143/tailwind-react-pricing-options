1feb7f3a545dc42a59785262b33e356f4423237f
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-10 20:24:30 +0800

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    
+<h1>prectice</h1>
     </>
   )
 }
